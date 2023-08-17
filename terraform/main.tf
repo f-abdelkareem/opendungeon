8a7fa2b1f821edd8e9ef893009898dff3b634a55
Fadhil Abdulkarim <fadh.abdelkareem@gmail.com>
2023-08-17 23:45:37 +0700

    init main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical, the official distributor of Ubuntu on AWS
+}
+
+resource "aws_security_group" "openvpn_sg" {
+  name        = "openvpn-sg"
+  description = "OpenVPN Access Server Security Group"
+
+  ingress {
+    from_port   = 22  # SSH
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 943  # OpenVPN Admin UI
+    to_port     = 943
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1194 # OpenVPN client connections (you can limit this CIDR range for added security)
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "openvpn_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.openvpn_sg.id]
+
+  tags = {
+    Name = "OpenVPN Server"
+  }
+}
