b35457f665f27e8e908db0bce34f49c4cd61c1b6
Fadhil Abdulkarim <fadh.abdelkareem@gmail.com>
2023-08-17 23:24:50 +0700

    add local-execute script

---
+++ scripts/local-provision.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e # Exit on error
+
+# Validate parameters (you might have environment parameters or others)
+# if [ "$#" -ne 1 ]; then
+#     echo "Usage: $0 <environment>"
+#     exit 1
+# fi
+
+# ENVIRONMENT="$1"
+
+# Terraform
+cd terraform
+
+# Initialize and apply Terraform changes
+echo "### Initializing Terraform ###"
+terraform init
+
+echo "### Applying Terraform ###" #for environment: $ENVIRONMENT ###"
+# terraform workspace select $ENVIRONMENT || terraform workspace new $ENVIRONMENT
+terraform apply -auto-approve
+
+# Extract necessary data for Ansible if needed (e.g., IPs of resources)
+# This assumes that your Terraform outputs some IP values that Ansible would use.
+INSTANCE_IP=$(terraform output instance_ip)
+
+cd ..
+
+# Ansible
+cd ansible
+
+# Execute Ansible playbook
+echo "### Running Ansible Playbook ###"
+ansible-playbook -i "$INSTANCE_IP," playbooks/deploy.yml
+
+cd ..
+
+echo "### Deployment Complete ###"
